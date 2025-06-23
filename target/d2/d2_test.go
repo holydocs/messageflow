34c880e4976a27b5a3ba95ab89c2bb656482c687
Denis Shevchenko <denis.shevchenko@outlook.com>
2025-06-24 00:50:06 +0300

    fix lint

--- target/d2/d2_test.go
+++ target/d2/d2_test.go
@@ -236,7 +236,6 @@ func TestFormatSchema(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -493,7 +492,6 @@ func TestRenderSchema(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
